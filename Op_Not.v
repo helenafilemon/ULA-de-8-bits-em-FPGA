c0a8c18cace8fac79cada1b540b4d317a0f1bda2
Davi Queiroz <daviengcomp@gmail.com>
2025-09-30 17:50:29 -0300

    Update Op_Not.v

--- Op_Not.v
+++ Op_Not.v
@@ -6,7 +6,7 @@
 	not Not1(S[1], A[1]);
 	not Not2(S[2], A[2]);
 	not Not3(S[3], A[3]);
-  not Not4(S[4], A[4]);
+    not Not4(S[4], A[4]);
 	not Not5(S[5], A[5]);
 	not Not6(S[6], A[6]);
 	not Not7(S[7], A[7]);
