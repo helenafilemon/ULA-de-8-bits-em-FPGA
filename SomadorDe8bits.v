b8a0f138e54d12d92bf7a1a7ad5e76b955d37e0c
Davi Queiroz <daviengcomp@gmail.com>
2025-09-30 17:49:32 -0300

    Update and rename SomadorDe8bits to SomadorDe8bits.v

--- SomadorDe8bits.v
+++ SomadorDe8bits.v
@@ -2,60 +2,60 @@ module somadorde4bits (S, Cout, A, B);
 	input [7:0] A, B;
 	output [7:0] S;
 	output Cout;
-	wire Cin_Cout1, Cin_Cout2, Cin_Cout3, Cin_Cout4, Cin_Cout5, Cin_Cout6, Cin_Cout7;
+	wire [7:0] Cin_Cout;
 	
-  Meiosomador bit1(
+  	Meiosomador bit1(
 		.A(A[0]),
 		.B(B[0]),
 		.S(S[0]),
-		.Cout(Cin_Cout1),
+	  	.Cout(Cin_Cout[1]),
 	);
 	SomadorPBL2 bit2(
 		.A(A[1]),
 		.B(B[1]),
-		.Cin(Cin_Cout1),
+		.Cin(Cin_Cout[1]),
 		.S(S[1]),
-		.Cout(Cin_Cout2),
+		.Cout(Cin_Cout[2]),
 	);
 	SomadorPBL2 bit3(
 		.A(A[2]),
 		.B(B[2]),
-		.Cin(Cin_Cout2),
+		.Cin(Cin_Cout[2]),
 		.S(S[2]),
-		.Cout(Cin_Cout3),
+		.Cout(Cin_Cout[3]),
 	);
 	SomadorPBL2 bit4(
 		.A(A[3]),
 		.B(B[3]),
-		.Cin(Cin_Cout3),
+		.Cin(Cin_Cout[3]),
 		.S(S[3]),
-		.Cout(Cin_Cout4),
+		.Cout(Cin_Cout[4]),
 	);
-  SomadorPBL2 bit5(
+ 	 SomadorPBL2 bit5(
 		.A(A[4]),
 		.B(B[4]),
-		.Cin(Cin_Cout4),
+		.Cin(Cin_Cout[4]),
 		.S(S[4]),
-		.Cout(Cin_Cout5),
+		.Cout(Cin_Cout[5]),
 	);
-  SomadorPBL2 bit6(
+ 	 SomadorPBL2 bit6(
 		.A(A[5]),
 		.B(B[5]),
-		.Cin(Cin_Cout5),
+		.Cin(Cin_Cout[5]),
 		.S(S[5]),
-		.Cout(Cin_Cout6),
+		.Cout(Cin_Cout[6]),
 	);
-  SomadorPBL2 bit7(
+ 	 SomadorPBL2 bit7(
 		.A(A[6]),
 		.B(B[6]),
-		.Cin(Cin_Cout6),
+		.Cin(Cin_Cout[6]),
 		.S(S[6]),
-		.Cout(Cin_Cout7),
+		.Cout(Cin_Cout[7]),
 	);
-  SomadorPBL2 bit8(
+ 	 SomadorPBL2 bit8(
 		.A(A[7]),
 		.B(B[7]),
-		.Cin(Cin_Cout7),
+		.Cin(Cin_Cout[7]),
 		.S(S[7]),
 		.Cout(Cout),
 	);
