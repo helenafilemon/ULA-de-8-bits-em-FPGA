e7cb07901919730c97de6240493499d37b6dd04d
Davi Queiroz <daviengcomp@gmail.com>
2025-10-20 15:55:15 -0300

    Update reg_2bitsRPN.v

--- fliflopD_Regis/reg_2bitsRPN.v
+++ fliflopD_Regis/reg_2bitsRPN.v
@@ -9,10 +9,10 @@ module registrador_de_estado_2bits (
     output [1:0] estado_atual
 );
     // Instancia um flip-flop para cada bit do estado
-    flip_flop_d_comportamental ff_bit0 (
+    flip_flop_d ff_bit0 (
         .clk(clk), .reset_n(reset_n), .d(proximo_estado[0]), .q(estado_atual[0])
     );
-    flip_flop_d_comportamental ff_bit1 (
+    flip_flop_d ff_bit1 (
         .clk(clk), .reset_n(reset_n), .d(proximo_estado[1]), .q(estado_atual[1])
     );
 endmodule
