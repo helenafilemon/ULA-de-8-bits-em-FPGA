8008447bf9bef41e165ed1d2a8c1bb69b68c3559
Davi Queiroz <daviengcomp@gmail.com>
2025-11-06 12:36:19 -0300

    Update pilhaRPN.v

--- pilhaRPN.v
+++ pilhaRPN.v
@@ -10,14 +10,14 @@ module pilhaRPN(
     output [7:0] saidaB  // Operando B (topo)
 );
 
-    wire enable_reg_B;
-    // 'enable_reg_B' e ativado se 'habilitaA' OU 'habilitaB' estiverem ativos.
-    or or_enable_B(enable_reg_B, habilitaA, habilitaB);
+    wire ativo_reg_B;
+    // 'ativo_reg_B' e ativado se 'habilitaA' OU 'habilitaB' estiverem ativos.
+    or enable_B(ativo_reg_B, habilitaA, habilitaB);
 
     // reg_A (fundo) recebe o valor antigo de B (saidaB)
     registrador8b reg_A(.D(saidaB),.clk(clk),.rst(rst),.habilita(habilitaB),.Q(saidaA));
     // reg_B (topo) recebe o novo dado (D)
-    registrador8b reg_B(.D(D),.clk(clk),.rst(rst),.habilita(enable_reg_B),.Q(saidaB));
+    registrador8b reg_B(.D(D),.clk(clk),.rst(rst),.habilita(ativo_reg_B),.Q(saidaB));
      
 endmodule
 
