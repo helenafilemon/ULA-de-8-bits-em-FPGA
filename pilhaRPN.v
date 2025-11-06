a52b7342df72497583fe0314aba58832fc41a37a
Davi Queiroz <daviengcomp@gmail.com>
2025-11-06 16:25:31 -0300

    Update pilhaRPN.v

--- pilhaRPN.v
+++ pilhaRPN.v
@@ -1,24 +1,20 @@
-// --- PILHA RPN (2 Niveis) ---
-// Proposito: Armazena os operandos A (fundo) e B (topo).
+// --- Pilha RPN ---
+// Armazena os dois operandos para a ULA.
 module pilhaRPN(
-    input [7:0] D,     // Novo dado a ser empurrado
+    input [7:0] D,       // Dado de entrada
     input clk,
     input rst,
-    input habilitaA,   // Sinal de 'push' vindo do estado 00
-    input habilitaB,   // Sinal de 'push' vindo do estado 01
-    output [7:0] saidaA, // Operando A (fundo)
-    output [7:0] saidaB  // Operando B (topo)
+    input habilitaA,     // Sinal de carga para o primeiro nivel
+    input habilitaB,     // Sinal de carga para o segundo nivel (push)
+    output [7:0] saidaA, // Operando A (fundo da pilha)
+    output [7:0] saidaB  // Operando B (topo da pilha)
 );
 
-    wire ativo_reg_B;
-    // 'ativo_reg_B' e ativado se 'habilitaA' OU 'habilitaB' estiverem ativos.
-    or enable_B(ativo_reg_B, habilitaA, habilitaB);
+    wire enable_reg_B;
+    or or_enable_B(enable_reg_B, habilitaA, habilitaB);
 
-    // reg_A (fundo) recebe o valor antigo de B (saidaB)
+    // Registradores que formam a pilha
     registrador8b reg_A(.D(saidaB),.clk(clk),.rst(rst),.habilita(habilitaB),.Q(saidaA));
-    // reg_B (topo) recebe o novo dado (D)
-    registrador8b reg_B(.D(D),.clk(clk),.rst(rst),.habilita(ativo_reg_B),.Q(saidaB));
+    registrador8b reg_B(.D(D),.clk(clk),.rst(rst),.habilita(enable_reg_B),.Q(saidaB));
      
 endmodule
-
-
