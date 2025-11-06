85d6ab45a5dcfb4e91c986ac67b80ca118ee35ae
Davi Queiroz <daviengcomp@gmail.com>
2025-11-06 01:22:43 -0300

    Update decodificador_op.v

--- decodificador_op.v
+++ decodificador_op.v
@@ -1,33 +1,33 @@
+// --- DECODIFICADOR DE OPCODE ---
+// Converte o Opcode[2:0] em 8 sinais de selecao (um para cada operacao)
 module decodificador_op(
     input [2:0] Op,
-    output sel_soma,
-    output sel_sub,
-    output sel_multi,
-    output sel_div,
-    output sel_and,
-    output sel_or,
-    output sel_xor,
-    output sel_not
-  // output op_valida // <--- REMOVIDO DA LISTA DE PORTAS
+    output sel_soma,  // 000
+    output sel_sub,   // 001
+    output sel_multi, // 010
+    output sel_div,   // 011
+    output sel_and,   // 100
+    output sel_or,    // 101
+    output sel_xor,   // 110
+    output sel_not    // 111
 );
 
     wire nOp0, nOp1, nOp2;
     
-    not(nOp0, Op[0]);
-    not(nOp1, Op[1]);
-    not(nOp2, Op[2]);
+    not not_op0(nOp0, Op[0]);
+    not not_op1(nOp1, Op[1]);
+    not not_op2(nOp2, Op[2]);
 
-    // --- Lógica de Decodificação (Inalterada) ---
-    and(sel_soma, nOp2, nOp1, nOp0); // 000
-    and(sel_sub,  nOp2, nOp1, Op[0]); // 001
-    and(sel_multi,  nOp2, Op[1], nOp0); // 010
-    and(sel_div,  nOp2, Op[1], Op[0]); // 011
-    and(sel_and,  Op[2], nOp1, nOp0); // 100
-    and(sel_or,   Op[2], nOp1, Op[0]); // 101
-    and(sel_xor,  Op[2], Op[1], nOp0); // 110
-    and(sel_not,  Op[2], Op[1], Op[0]); // 111
-
-    // --- LÓGICA 'op_valida' REMOVIDA ---
-    // or(op_valida, sel_soma, sel_sub, sel_multi, sel_div, sel_and, sel_or, sel_xor, sel_not);
+    // Logica de decodificacao (1-hot)
+    and and_soma(sel_soma, nOp2, nOp1, nOp0); // 000
+    and and_sub(sel_sub,  nOp2, nOp1, Op[0]); // 001
+    and and_multi(sel_multi,  nOp2, Op[1], nOp0); // 010
+    and and_div(sel_div,  nOp2, Op[1], Op[0]); // 011
+    and and_and(sel_and,  Op[2], nOp1, nOp0); // 100
+    and and_or(sel_or,   Op[2], nOp1, Op[0]); // 101
+    and and_xor(sel_xor,  Op[2], Op[1], nOp0); // 110
+    and and_not(sel_not,  Op[2], Op[1], Op[0]); // 111
 
 endmodule
+
+
